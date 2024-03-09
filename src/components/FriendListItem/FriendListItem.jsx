7f8e644afa3e7f20f2dbe9320b6df7948c95307c
AnnaViktorivna <anya.zav94@gmail.com>
2024-03-09 20:16:25 +0100

    add-2-task-ready

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,27 @@
+import css from "./FriendListItem.module.css";
+import clsx from "clsx";
+
 const FriendListItem = ({ friend }) => {
+  const isFriendOnlineCheck = friend.isOnline === false;
   return (
-    <li>
-      <div>
-        <img src={friend.avatar} alt='Avatar' width='48' />
-        <p>{friend.name}</p>
-        <p>{friend.isOnline ? "Online" : "Offline"}</p>
+    <li className={css.friendItem}>
+      <div className={css.wrapFriendsInfo}>
+        <img
+          className={css.friendAvatar}
+          src={friend.avatar}
+          alt='Avatar'
+          width='48'
+        />
+        <p className={css.friendName}>{friend.name}</p>
+        <p
+          className={clsx(css.friendIsOnline, {
+            [css.friendIsOffline]: isFriendOnlineCheck,
+          })}
+        >
+          {friend.isOnline ? "Online" : "Offline"}
+        </p>
       </div>
     </li>
   );
 };
 export default FriendListItem;
-
-// isOnline ? css.online : css.offline}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,31 @@
-import "./Profile.css";
+import css from "./Profile.module.css";
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className='wrap'>
+    <div className={css.wrap}>
       {" "}
-      <div className='wrapUserInfo'>
+      <div className={css.wrapUserInfo}>
         {" "}
-        <img className='wrapUserInfoImg' src={image} alt='User avatar' />
-        <p className='wrapUserInfoName'>{name}</p>{" "}
-        <p className='wrapUserInfoTag'>@{tag}</p>
-        <p className='wrapUserInfoLocation'>{location}</p>{" "}
+        <img className={css.wrapUserInfoImg} src={image} alt='User avatar' />
+        <p className={css.wrapUserInfoName}>{name}</p>{" "}
+        <p className={css.wrapUserInfoTag}>@{tag}</p>
+        <p className={css.wrapUserInfoLocation}>{location}</p>{" "}
       </div>{" "}
-      <ul className='listStatsUser'>
+      <ul className={css.listStatsUser}>
         {" "}
-        <li className='listStatsUserItem'>
+        <li className={css.listStatsUserItem}>
           <span>Followers</span>
-          <span>{stats.followers}</span>{" "}
+          <span className={css.listStatsItemElement}>
+            {stats.followers}
+          </span>{" "}
         </li>{" "}
-        <li className='listStatsUserItem'>
+        <li>
           <span>Views</span>
-          <span>{stats.views}</span>{" "}
+          <span className={css.listStatsItemElement}>{stats.views}</span>{" "}
         </li>{" "}
-        <li className='listStatsUserItem'>
+        <li>
           <span>Likes</span>
-          <span>{stats.likes}</span>{" "}
+          <span className={css.listStatsItemElement}>{stats.likes}</span>{" "}
         </li>{" "}
       </ul>
     </div>
